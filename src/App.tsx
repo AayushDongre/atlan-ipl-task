cba93451674bb43666bfb0bb5cd452d887487d38
AayushDongre <aayushdongre4@gmail.com>
2021-03-06 17:47:25 +0530

    feat: add navigation functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import TopBar from "./Components/NavBar/NavBar";
 import "./App.scss";
 import PlayerCard from "./Components/PlayerCard/PlayerCard";
@@ -6,123 +6,39 @@ import TeamCard from "./Components/TeamCard/TeamCard";
 import MatchCard from "./Components/MatchCard/MatchCard";
 
 function App() {
+  const [currentPath, setCurrentPath] = useState("players");
+
   return (
     <div className="App">
       <div className="wrapper">
-        <TopBar />
+        <TopBar currentPath={currentPath} setCurrentPath={setCurrentPath} />
         <div className="cardList">
-          <PlayerCard
-            country="India"
-            name="V. Kohli"
-            dob="05-07-2017"
-            dominantArm="Right"
-          />
-          <PlayerCard
-            country="India"
-            name="V. Kohli"
-            dob="05-07-2017"
-            dominantArm="Right"
-          />
-          <PlayerCard
-            country="India"
-            name="V. Kohli"
-            dob="05-07-2017"
-            dominantArm="Right"
-          />
-          <PlayerCard
-            country="India"
-            name="V. Kohli"
-            dob="05-07-2017"
-            dominantArm="Right"
-          />
-          <PlayerCard
-            country="India"
-            name="V. Kohli"
-            dob="05-07-2017"
-            dominantArm="Right"
-          />
-          <PlayerCard
-            country="India"
-            name="V. Kohli"
-            dob="05-07-2017"
-            dominantArm="Right"
-          />
-          <TeamCard
-            teamName="Kings XI Punjab"
-            awayPercent={47.2}
-            homePercent={62.5}
-          />
-          <TeamCard
-            teamName="Kings XI Punjab"
-            awayPercent={47.2}
-            homePercent={62.5}
-          />
-          <TeamCard
-            teamName="Kings XI Punjab"
-            awayPercent={47.2}
-            homePercent={62.5}
-          />
-          <TeamCard
-            teamName="Kings XI Punjab"
-            awayPercent={47.2}
-            homePercent={62.5}
-          />
-          <TeamCard
-            teamName="Kings XI Punjab"
-            awayPercent={47.2}
-            homePercent={62.5}
-          />
-          <TeamCard
-            teamName="Kings XI Punjab"
-            awayPercent={47.2}
-            homePercent={62.5}
-          />
-          <TeamCard
-            teamName="Kings XI Punjab"
-            awayPercent={47.2}
-            homePercent={62.5}
-          />
-          <TeamCard
-            teamName="Kings XI Punjab"
-            awayPercent={47.2}
-            homePercent={62.5}
-          />
-          <MatchCard
-            winningTeam="Sunrisers Hyderabad"
-            losingTeam="Royal Challengers Bangalore"
-            loaction="Hyderabad"
-            date="05-04-2017"
-          />
-          <MatchCard
-            winningTeam="Sunrisers Hyderabad"
-            losingTeam="Royal Challengers Bangalore"
-            loaction="Hyderabad"
-            date="05-04-2017"
-          />
-          <MatchCard
-            winningTeam="Sunrisers Hyderabad"
-            losingTeam="Royal Challengers Bangalore"
-            loaction="Hyderabad"
-            date="05-04-2017"
-          />
-          <MatchCard
-            winningTeam="Sunrisers Hyderabad"
-            losingTeam="Royal Challengers Bangalore"
-            loaction="Hyderabad"
-            date="05-04-2017"
-          />
-          <MatchCard
-            winningTeam="Sunrisers Hyderabad"
-            losingTeam="Royal Challengers Bangalore"
-            loaction="Hyderabad"
-            date="05-04-2017"
-          />
-          <MatchCard
-            winningTeam="Sunrisers Hyderabad"
-            losingTeam="Royal Challengers Bangalore"
-            loaction="Hyderabad"
-            date="05-04-2017"
-          />
+          {currentPath === "players" &&
+            [1, 2, 3, 4, 5, 6, 7, 8].map(() => (
+              <PlayerCard
+                country="India"
+                name="V. Kohli"
+                dob="05-07-2017"
+                dominantArm="Right"
+              />
+            ))}
+          {currentPath === "teams" &&
+            [1, 2, 3, 4, 5, 6, 7, 8].map(() => (
+              <TeamCard
+                teamName="Kings XI Punjab"
+                awayPercent={47.2}
+                homePercent={62.5}
+              />
+            ))}
+          {currentPath === "matches" &&
+            [1, 2, 3, 4, 5, 6, 7, 8].map(() => (
+              <MatchCard
+                winningTeam="Sunrisers Hyderabad"
+                losingTeam="Royal Challengers Bangalore"
+                loaction="Hyderabad"
+                date="05-04-2017"
+              />
+            ))}
         </div>
       </div>
     </div>
--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -3,16 +3,20 @@ import NavBarButton from "../NavBarButton/NavBarButton";
 
 import "./NavBar.scss";
 
-const NavBar: React.FC = () => {
+export interface NavBarProps {
+  currentPath: string;
+  setCurrentPath: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const NavBar: React.FC<NavBarProps> = (props) => {
   const paths = ["players", "teams", "matches"];
-  const [currentPath, setCurrentPath] = useState("players");
 
   return (
     <div className="navBar">
       <div className="navbarContent">
         {paths.map((path) => (
-          <div onClick={() => setCurrentPath(path)}>
-            <NavBarButton path={path} active={currentPath === path} />
+          <div onClick={() => props.setCurrentPath(path)}>
+            <NavBarButton path={path} active={props.currentPath === path} />
           </div>
         ))}
       </div>
